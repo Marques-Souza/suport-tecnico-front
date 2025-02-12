427f9b037166f778794dd977337e19b9f2e0d948
Marques-Souza <marques.wsouza357@gmail.com>
2025-02-12 16:11:41 -0300

    Html e Css para Tecnico-List

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from '../../home/models/tecnico';
+
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+ELEMENT_DATA: Tecnico[] = [
+  {
+    id: 1,
+    nome: 'Marques Souza',
+    cpf: '035.896.461-01',
+    email: 'marques@admin.com',
+    senha: '123456',
+    perfis: ['0'],
+    dataCriacao: '10/02/2024'
+  }
+]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
+
+
