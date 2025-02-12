a7219fd5e38e1481afe79c767fc52d1ccac54b84
Marques-Souza <marques.wsouza357@gmail.com>
2025-02-12 15:22:26 -0300

    Component header criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,14 +31,16 @@ import{MatCardModule} from '@angular/material/card';
 //Components do Projeto
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './component/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+  
   ],
   imports: [
     BrowserModule,
