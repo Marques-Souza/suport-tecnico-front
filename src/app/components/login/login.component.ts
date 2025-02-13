fba43adccfd55fe719490840551055108d8d2397
Marques-Souza <marques.wsouza357@gmail.com>
2025-02-13 09:59:50 -0300

    guard de rotas criado

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Credenciais } from '../home/models/credenciais';
 import { FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Component({
@@ -19,20 +20,27 @@ export class LoginComponent implements OnInit {
     email = new FormControl(null, Validators.email);
     senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toastr: ToastrService) { }
+  constructor(
+    private toastr: ToastrService,
+    private service: AuthService
+  ) { }
 
   ngOnInit(): void {
 
   }
 
   logar() {
-    this.toastr.error('Usuário e/ou senha inválidos', 'Login');
-    this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfullLogin(resposta.headers.get('Authorization').substring(7));
+     
+    }, () => {
+      this.toastr.error('Usúario e/ou senha inválidos');
+    })
     
 
   }
 
   validarCampos(): boolean {
-    return this.email.valid && this.senha.valid
+       return this.email.valid && this.senha.valid
     }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Credenciais } from '../components/home/models/credenciais';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from '../components/home/config/api.config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  authenticate(creds: Credenciais ){
+    return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
+      observe: 'response',
+      responseType: 'text'
+    })
+  }
+
+  successfullLogin(authToken: string){
+    localStorage.setItem('token', authToken);
+  }
 }
