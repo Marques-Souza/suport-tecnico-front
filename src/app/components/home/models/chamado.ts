e9953e9739b6c2cddedf5981940e75ae172c995a
Marques-Souza <marques.wsouza357@gmail.com>
2025-02-21 15:02:14 -0300

    Serviço para criar Chamado implementado

--- src/app/components/home/models/chamado.ts
+++ src/app/components/home/models/chamado.ts
@@ -1,4 +1,4 @@
-export interface Chamado{
+export interface Chamado {
 
     id?: any
     dataAbertura?: string;
@@ -6,7 +6,7 @@ export interface Chamado{
     prioridade: string;
     status: string;
     titulo: string;
-    descricao: string;
+    observacoes: string;
     tecnico: any;
     cliente: any;
     nomeCliente: string;
--- src/app/services/chamado.service.ts
+++ src/app/services/chamado.service.ts
@@ -11,9 +11,15 @@ export class ChamadoService {
 
   constructor(private http: HttpClient) {
 
-   }
+  }
 
-   findAll(): Observable<Chamado[]>{
+  findAll(): Observable<Chamado[]> {
     return this.http.get<Chamado[]>(`${API_CONFIG.baseUrl}/chamados`);
-}
+
+  }
+
+  create(chamado: Chamado): Observable<Chamado> {
+    return this.http.post<Chamado>(`${API_CONFIG.baseUrl}/chamados`, chamado);
+  }
+
 }
