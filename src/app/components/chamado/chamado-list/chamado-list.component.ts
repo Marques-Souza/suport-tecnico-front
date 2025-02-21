d235f1a941093179f07fc0e1d85fc9089b007640
Marques-Souza <marques.wsouza357@gmail.com>
2025-02-21 12:36:22 -0300

    Html e Css para component CHAMADO-LIST

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chamado } from '../../home/models/chamado';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-chamado-list',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChamadoListComponent implements OnInit {
 
+  ELEMENT_DATA: Chamado[] = [
+    {
+      id: 1,
+      dataAbertura: '2021-04-21',
+      dataFechamento: '2021-04-21',
+      prioridade: 'ALTA',
+      status: 'ANDAMENTO',
+      titulo: 'Chamado 1',
+      descricao: 'Descricao do chamado 1',
+      tecnico: '1',
+      cliente: '6',
+      nomeCliente: 'marques souza',
+      nomeTecnico: 'yore'
+  
+    }
+
+  ]
+  
+    displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+    dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+  
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 
 }
